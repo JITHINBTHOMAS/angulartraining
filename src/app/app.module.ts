e95fea9f600f6855dd64f32ab1ccfda9e47c91e1
JITHINBTHOMAS <JITHINBTHOMAS>
2024-10-26 11:23:25 +0530

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataComponent } from './data/data.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { StrawhatsComponent } from './strawhats/strawhats.component';
+import { HttpComponent } from './http/http.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataComponent,
+    StrawhatsComponent,
+    HttpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
